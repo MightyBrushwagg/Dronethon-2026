429e9f90a9c7e4d352ec14965f3c1ee6dfe6fd7b
Xavier <xavier.parker.24@ucl.ac.uk>
2025-12-06 14:26:57 +0000

    Update control.py

--- controllers/drone_controller/control.py
+++ controllers/drone_controller/control.py
@@ -48,13 +48,8 @@ class Control():
         self.integral = {'roll': 0, 'pitch': 0, 'yaw': 0, 'vertical': 0}
         self.prev_error = {'roll': 0, 'pitch': 0, 'yaw': 0, 'vertical': 0}
     
-<<<<<<< Updated upstream
-        self.k_vertical_thrust = 68.5  # with this thrust, the drone lifts.
-        self.k_vertical_offset = 0.6 
-=======
         self.k_vertical_thrust = 68.5;  # with this thrust, the drone lifts.
         self.k_vertical_offset = 0.6; 
->>>>>>> Stashed changes
         
         
     def process_signal(self, vel):
