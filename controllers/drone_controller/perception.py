a8230003c2b9d6071a0abfdd3c0de5092ce4cff2
Xavier <xavier.parker.24@ucl.ac.uk>
2025-12-06 12:40:10 +0000

    Update perception.py

--- controllers/drone_controller/perception.py
+++ controllers/drone_controller/perception.py
@@ -3,6 +3,63 @@ from controller import Robot, Camera, Compass, GPS, Gyro, InertialUnit, Keyboard
 import numpy as np
 import torch
 
+from controller import Robot
+
+class LidarPerception:
+    def __init__(self, robot: Robot, lidar_name="lidar"):
+        self.robot = robot
+        self.timestep = int(robot.getBasicTimeStep())
+
+        # Get and enable the lidar
+        self.lidar = robot.getDevice(lidar_name)
+        self.lidar.enable(self.timestep)
+        self.lidar.enablePointCloud()
+
+        # Store specs
+        self.num_layers = self.lidar.getNumberOfLayers()
+        self.num_points = self.lidar.getHorizontalResolution()
+        self.fov = self.lidar.getFov()
+
+        print(f"[LIDAR] Layers: {self.num_layers}, Points per layer: {self.num_points}")
+
+    def get_ranges(self):
+        """
+        Returns the full 2D array of distances:
+        shape = [num_layers][num_points]
+        """
+        ranges = self.lidar.getRangeImage()
+        return ranges   # already a Python list of lists
+
+    def get_front_left_right(self):
+        """
+        Get distances in three directions:
+        - front
+        - left
+        - right
+        (for single-layer lidar)
+        """
+        ranges = self.lidar.getRangeImage()
+        layer = ranges[0]  # single layer
+
+        mid = self.num_points // 2
+        left = self.num_points // 4
+        right = 3 * self.num_points // 4
+
+        return {
+            "front": layer[mid],
+            "left": layer[left],
+            "right": layer[right]
+        }
+
+    def get_min_distance(self):
+        """
+        Shortest obstacle distance detected in front hemisphere.
+        """
+        ranges = self.lidar.getRangeImage()[0]
+        return min(ranges)
+
+
+
 class Perception():
     def __init__(self, robot, timestep):
         self.robot = robot
@@ -35,9 +92,18 @@ class Perception():
         """
         Get the current state vector for MPPI: [x, y, z, roll, pitch, yaw, vx, vy, vz, wx, wy, wz]
         """
-        # state = np.array([x, y, z, roll, pitch, yaw, vx, vy, vz, wx, wy, wz])
-        # return state
-        pass
+        # position
+        position = self.gps.getValues()  # [x, y, z]
+        orientation = self.imu.getRollPitchYaw()  # [roll, pitch, yaw]
+        angular_velocity = self.gyro.getValues()  # [wx, wy, wz
+        linear_velocity = self.imu.getLinearVelocity()  # [vx, vy, vz]
+        return np.array([
+            position[0], position[1], position[2],
+            orientation[0], orientation[1], orientation[2],
+            linear_velocity[0], linear_velocity[1], linear_velocity[2],
+            angular_velocity[0], angular_velocity[1], angular_velocity[2]
+        ])
+
     
     def get_obstacle_distances(self):
         """
@@ -46,6 +112,8 @@ class Perception():
         # user lidar or range finders (ray casting)
         # distances = {"front": front_distance, "left": left_distance, "right": right_distance, "back": back_distance, "up": up_distance, "down": down_distance}
         # return distances
+
+
         pass
         
     
