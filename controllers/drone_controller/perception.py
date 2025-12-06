61376554451ea92e22099345e02da27160817141
Mr-Moody <118393511+Mr-Moody@users.noreply.github.com>
2025-12-06 12:20:45 +0000

    Added WorldAwareRewardModel

--- controllers/drone_controller/perception.py
+++ controllers/drone_controller/perception.py
@@ -17,3 +17,38 @@ class Perception():
 
         self.camera = self.robot.getDevice("camera")
         self.camera.enable(self.timestep)
+        
+
+        # setup range finders or lidar for obstacle detection
+    
+    
+    def get_state_vector(self):
+        """
+        Get the current state vector for MPPI: [x, y, z, roll, pitch, yaw, vx, vy, vz, wx, wy, wz]
+        """
+        # state = np.array([x, y, z, roll, pitch, yaw, vx, vy, vz, wx, wy, wz])
+        # return state
+        pass
+    
+    def get_obstacle_distances(self):
+        """
+        Get distance to nearest obstacles in multiple directions.
+        """
+        # user lidar or range finders (ray casting)
+        # distances = {"front": front_distance, "left": left_distance, "right": right_distance, "back": back_distance, "up": up_distance, "down": down_distance}
+        # return distances
+        pass
+        
+    
+    def get_camera_depth_map(self):
+        """
+        Get camera depth map.
+        """
+        # return {
+        #     "image": image,
+        #     "edges": edges,
+        #     "has_obstacle": np.sum(edges) > 1000  # Threshold for obstacle detection
+        # }
+        pass
+
+    
