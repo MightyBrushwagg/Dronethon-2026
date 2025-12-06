1a1098db0943b83306f4ba86728add2a909329b1
Mr-Moody <118393511+Mr-Moody@users.noreply.github.com>
2025-12-06 11:33:00 +0000

    Separated classes

--- controllers/drone_controller/control.py
+++ controllers/drone_controller/control.py
@@ -1,3 +1,30 @@
 class Control():
-    def __init__(self):
-        pass
+    def __init__(self, robot, timestep):
+        self.robot = robot
+        self.timestep = timestep
+        self.camera_roll_motor = self.robot.getDevice("camera roll")
+        self.camera_pitch_motor = self.robot.getDevice("camera pitch")
+        self.camera_roll_motor.setPosition(0)
+        self.camera_pitch_motor.setPosition(0)
+        self.camera_roll_motor.enable(self.timestep)
+        self.camera_pitch_motor.enable(self.timestep)
+
+        self.front_left_motor = self.robot.getDevice("front left propeller")
+        self.front_right_motor = self.robot.getDevice("front right propeller")
+        self.rear_left_motor = self.robot.getDevice("rear left propeller")
+        self.rear_right_motor = self.robot.getDevice("rear right propeller")
+
+        self.front_left_motor.setPosition(float('inf'))
+        self.front_right_motor.setPosition(float('inf'))
+        self.rear_left_motor.setPosition(float('inf'))
+        self.rear_right_motor.setPosition(float('inf'))
+
+        self.front_left_motor.setVelocity(1.0)
+        self.front_right_motor.setVelocity(1.0)
+        self.rear_left_motor.setVelocity(1.0)
+        self.rear_right_motor.setVelocity(1.0)
+
+        self.front_left_motor.enable(self.timestep)
+        self.front_right_motor.enable(self.timestep)
+        self.rear_left_motor.enable(self.timestep)
+        self.rear_right_motor.enable(self.timestep)
--- controllers/drone_controller/perception.py
+++ controllers/drone_controller/perception.py
@@ -1,3 +1,19 @@
 class Perception():
-    def __init__(self):
-        pass
+    def __init__(self, robot, timestep):
+        self.robot = robot
+        self.timestep = timestep
+
+        self.imu = self.robot.getDevice("inertial unit")
+        self.imu.enable(self.timestep)
+
+        self.gps = self.robot.getDevice("gps")
+        self.gps.enable(self.timestep)
+
+        self.compass = self.robot.getDevice("compass")
+        self.compass.enable(self.timestep)
+
+        self.gyro = self.robot.getDevice("gyro")
+        self.gyro.enable(self.timestep)
+
+        self.camera = self.robot.getDevice("camera")
+        self.camera.enable(self.timestep)
